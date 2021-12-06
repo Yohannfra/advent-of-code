29c5957cdebdc98c97dd96349ecb7d19c63449e0
Yohann <yohannassouline@hotmail.fr>
2021-12-06 13:28:02 +0100

    add part2 of day6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,7 +3,9 @@ use std::fs;
 
 fn parse_file(fp: &str) -> Vec<i32> {
     let fc: String = fs::read_to_string(fp).expect("Could not read the file");
-    let line: Vec<&str> = (fc.split("\n").collect::<Vec<&str>>())[0].split(",").collect();
+    let line: Vec<&str> = (fc.split("\n").collect::<Vec<&str>>())[0]
+        .split(",")
+        .collect();
 
     let mut fishes: Vec<i32> = Vec::new();
 
@@ -11,18 +13,6 @@ fn parse_file(fp: &str) -> Vec<i32> {
         fishes.push(i.parse::<i32>().unwrap());
     }
     fishes
-}
-
-fn print_fishes_state(start: String, fishes: &Vec<i32>) {
-    print!("{}", start);
-
-    for i in 0..=fishes.len() - 1 {
-        print!("{}", fishes[i]);
-        if i != fishes.len() - 1 {
-            print!(",");
-        }
-    }
-    print!("\n");
 }
 
 fn reproduce_fishes(fishes: &mut Vec<i32>) {
@@ -39,16 +29,41 @@ fn reproduce_fishes(fishes: &mut Vec<i32>) {
     fishes.append(&mut new_fishes);
 }
 
+fn reproduce_fishes_2(fishes: &mut Vec<i64>) {
+    for i in 1..=9 {
+        let tmp: i64 = fishes[i];
+        fishes[i] = 0;
+        fishes[i - 1] += tmp;
+    }
+    let tmp: i64 = fishes[0];
+    fishes[0] = 0;
+    fishes[7] += tmp;
+    fishes[9] += tmp;
+}
+
+fn part2(fp: &str) {
+    let all_fishes: Vec<i32> = parse_file(fp);
+
+    let mut fishes: Vec<i64> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for f in all_fishes {
+        fishes[f as usize + 1] += 1
+    }
+
+    for days in 1..=256 {
+        reproduce_fishes_2(&mut fishes);
+    }
+
+    let sum: i64 = fishes.iter().sum();
+    println!("Total = {}", sum);
+}
+
 fn part1(fp: &str) {
     let mut fishes: Vec<i32> = parse_file(fp);
 
-    // print_fishes_state("Initial State:\t\t".to_string(), &fishes);
-
     for days in 1..=80 {
         reproduce_fishes(&mut fishes);
-        // print_fishes_state(format!("After\t{} {}\t\t", days, if days == 1 { "day" } else { "days" }), &fishes);
     }
-
     println!("Total = {}", fishes.len());
 }
 
@@ -59,5 +74,6 @@ fn main() {
         println!("USAGE: day05 input.txt");
         std::process::exit(1);
     }
-    part1(&args[1]);
+    // part1(&args[1]);
+    part2(&args[1]);
 }
