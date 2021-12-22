44a5b7770d08db81ceabd148d949febb0f07174b
Yohann <yohannassouline@hotmail.fr>
2021-12-22 23:03:13 +0100

    add part2 of day10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -3,6 +3,9 @@ use std::fs;
 use std::io::{self, prelude::*, BufReader};
 use std::path::Path;
 
+const OPEN_CHARS: &str = "([{<";
+const CLOSE_CHARS: &str = ")]}>";
+
 fn parse_file(fp: &Path) -> Result<Vec<String>, io::Error> {
     let f = fs::File::open(fp)?;
 
@@ -27,9 +30,6 @@ fn part1(fp: &Path) {
     let mut sum_of_errors: u64 = 0;
 
     let mut chars_vec: Vec<char> = Vec::new();
-
-    const OPEN_CHARS: &str = "([{<";
-    const CLOSE_CHARS: &str = ")]}>";
 
     for l in lines {
         for c in l.chars() {
@@ -56,9 +56,88 @@ fn part1(fp: &Path) {
     println!("{}", sum_of_errors);
 }
 
+fn is_valid_line(l: &str) -> bool {
+    let mut chars_vec: Vec<char> = Vec::new();
+
+    for c in l.chars() {
+        if OPEN_CHARS.contains(c) {
+            chars_vec.push(c);
+        } else {
+            let last_char_in: char = *chars_vec.last().unwrap();
+            if OPEN_CHARS.find(last_char_in) != CLOSE_CHARS.find(c) {
+                return false;
+            }
+            chars_vec.pop();
+        }
+    }
+    true
+}
+
+fn complete_line(l: &str) -> String {
+    let mut eol: String = String::new();
+
+    let mut chars_vec: Vec<char> = Vec::new();
+
+    for c in l.chars() {
+        if OPEN_CHARS.contains(c) {
+            chars_vec.push(c);
+        } else {
+            let last_char_in: char = *chars_vec.last().unwrap();
+            if OPEN_CHARS.find(last_char_in) != CLOSE_CHARS.find(c) {
+                continue;
+            }
+            chars_vec.pop();
+        }
+    }
+
+    while !chars_vec.is_empty() {
+        let c: char = *chars_vec.last().unwrap();
+        chars_vec.pop();
+        match c {
+            '[' => eol.push(']'),
+            '<' => eol.push('>'),
+            '{' => eol.push('}'),
+            '(' => eol.push(')'),
+            _ => panic!("Invalid char"),
+        }
+    }
+    eol
+}
+
+fn count_completion_score(s: &str) -> u64 {
+    let mut n: u64 = 0;
+
+    for c in s.chars() {
+        n *= 5;
+        n += match c {
+            ')' => 1,
+            ']' => 2,
+            '}' => 3,
+            '>' => 4,
+            _ => panic!("Invalid char"),
+        }
+    }
+    n
+}
+
 #[allow(dead_code)]
 fn part2(fp: &Path) {
+    let lines: Vec<String> = match parse_file(fp) {
+        Ok(v) => v,
+        Err(e) => panic!("{}", e),
+    };
 
+    let mut all_scores: Vec<u64> = Vec::new();
+
+    for l in lines {
+        if !is_valid_line(&l) {
+            continue;
+        }
+        let end_of_line: String = complete_line(&l);
+        all_scores.push(count_completion_score(&end_of_line));
+    }
+    all_scores.sort();
+    println!("{:?}", all_scores[all_scores.len() / 2]);
 }
 
 fn main() {
@@ -69,6 +148,6 @@ fn main() {
         std::process::exit(1);
     }
 
-    part1(Path::new(&args[1]));
-    // part2(Path::new(&args[1]));
+    // part1(Path::new(&args[1]));
+    part2(Path::new(&args[1]));
 }
