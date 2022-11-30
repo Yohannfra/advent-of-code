0d72a233109e9ba900b0533d6a17351eee5e2c7b
Yohann Assouline <yohannassouline@hotmail.fr>
2022-11-30 13:20:34 +0100

    Finish part 1 of day 2 2018

--- 2018/src/days/day2.rs
+++ 2018/src/days/day2.rs
@@ -1,5 +1,26 @@
-pub fn part1(_fp: &str) -> Option<String> {
-    Some("".to_owned())
+use crate::utils;
+
+pub fn part1(fp: &str) -> Option<String> {
+    let fc = utils::read_file_to_vec(fp).unwrap();
+    let mut num_2 = 0;
+    let mut num_3 = 0;
+
+
+    for l in fc {
+        let mut has_num2 = false;
+        let mut has_num3 = false;
+        for c in l.chars() {
+            if l.matches(c).count() == 2 && !has_num2 {
+                num_2 += 1;
+                has_num2 = true;
+            }
+            if l.matches(c).count() == 3 && !has_num3 {
+                num_3 += 1;
+                has_num3 = true;
+            }
+        }
+    }
+    Some((num_3 * num_2).to_string())
 }
 
 pub fn part2(_fp: &str) -> Option<String> {
