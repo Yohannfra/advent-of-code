2139edc1f1ef8fb10e24d150bebadfb5e10257e7
Yohann Assouline <yohannassouline@hotmail.fr>
2022-11-30 13:06:51 +0100

    Finish part 2 of day 1 2018

--- 2018/src/days/day1.rs
+++ 2018/src/days/day1.rs
@@ -15,6 +15,23 @@ pub fn part1(fp: &str) -> Option<String> {
     Some(freq.to_string())
 }
 
-pub fn part2(_fp: &str) -> Option<String> {
-    Some("".to_owned())
+pub fn part2(fp: &str) -> Option<String> {
+    let fc = utils::read_file_to_vec(fp).unwrap();
+    let mut past_freqs: Vec<i32> = vec![0];
+    let mut freq = 0;
+
+    loop {
+        for l in &fc {
+            freq += match l.chars().nth(0).unwrap() {
+                '+' => l.clone()[1..].parse::<i32>().unwrap(),
+                '-' => - l.clone()[1..].parse::<i32>().unwrap(),
+                _ => panic!(),
+            };
+
+            if past_freqs.iter().find(|x| **x == freq).is_some() {
+                return Some(freq.to_string());
+            }
+            past_freqs.push(freq);
+        }
+    }
 }
