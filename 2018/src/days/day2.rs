79e5775da800a646ffee7ecd2b869ba4625e8e30
Yohann Assouline <yohannassouline@hotmail.fr>
2022-12-01 00:14:55 +0100

    Finish part 2 of day 2 2018

--- 2018/src/days/day2.rs
+++ 2018/src/days/day2.rs
@@ -23,6 +23,21 @@ pub fn part1(fp: &str) -> Option<String> {
     Some((num_3 * num_2).to_string())
 }
 
-pub fn part2(_fp: &str) -> Option<String> {
-    Some("".to_owned())
+pub fn part2(fp: &str) -> Option<String> {
+    let fc = utils::read_file_to_vec(fp).unwrap();
+
+    for l1 in &fc {
+        for l2 in &fc {
+            let mut nb_diff = 0;
+            for i in 0..l1.len() {
+                if l1.chars().nth(i) != l2.chars().nth(i) {
+                    nb_diff += 1;
+                }
+            }
+            if nb_diff == 1 {
+                return Some(format!("{} {}", l1, l2));
+            }
+        }
+    }
+    None
 }
