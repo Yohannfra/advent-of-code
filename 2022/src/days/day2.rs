d45b8091ae1730a5670b4a9b44d98ac50ed9e8f7
Yohann Assouline <yohannassouline@hotmail.fr>
2022-12-02 11:42:34 +0100

    Finish day 2 of 2022

--- 2022/src/days/day2.rs
+++ 2022/src/days/day2.rs
@@ -1,7 +1,86 @@
-pub fn part1(_fp: &str) -> Option<String> {
-    Some("".to_owned())
+use crate::utils;
+
+fn rock_paper_scisor_win(my: &str, op: &str) -> i32 { // 0:loose 6: win 3:tie
+    if my == "Z" && op == "C" ||  my == "Y" && op == "B" || my == "X" && op == "A" {
+        return 3; // tie
+    }
+
+    if my == "Y" && op == "A" { // Paper vs Rock
+        return 6;
+    }
+    if my == "X" && op == "C" {
+        return 6;
+    }
+
+    if my == "Z" && op == "B" {
+        return 6;
+    }
+
+    0
 }
 
-pub fn part2(_fp: &str) -> Option<String> {
-    Some("".to_owned())
+pub fn part1(fp: &str) -> Option<String> {
+    let fc = utils::read_file_to_vec(fp).unwrap();
+
+    let mut score = 0;
+
+    for l in fc {
+        let mut sp = l.split(" ");
+        let op = sp.next().unwrap();
+        let my = sp.next().unwrap();
+
+        let shape_score = match my {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => panic!()
+        };
+        score += match rock_paper_scisor_win(my, op) {
+            3 => 3 + shape_score,
+            6 => 6 + shape_score,
+            _ => 0 + shape_score,
+        };
+    }
+    Some(score.to_string())
+}
+
+pub fn part2(fp: &str) -> Option<String> {
+    let fc = utils::read_file_to_vec(fp).unwrap();
+
+    let mut score = 0;
+
+    for l in fc {
+        let mut sp = l.split(" ");
+        let op = sp.next().unwrap();
+        let res = sp.next().unwrap();
+
+        let my = match (op, res) {
+            ("A", "X") => "Z",
+            ("A", "Y") => "X",
+            ("A", "Z") => "Y",
+
+            ("B", "X") => "X",
+            ("B", "Y") => "Y",
+            ("B", "Z") => "Z",
+
+            ("C", "X") => "Y",
+            ("C", "Y") => "Z",
+            ("C", "Z") => "X",
+            _ => panic!(),
+        };
+
+        let shape_score = match my {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => panic!()
+        };
+        score += match rock_paper_scisor_win(my, op) {
+            3 => 3 + shape_score,
+            6 => 6 + shape_score,
+            _ => 0 + shape_score,
+        };
+
+    }
+    Some(score.to_string())
 }
