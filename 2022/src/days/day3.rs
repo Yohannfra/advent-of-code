2c99602d7582d8a655c97329194919a757ea83c8
Yohann Assouline <yohannassouline@hotmail.fr>
2022-12-03 10:22:50 +0100

    Finish day 3 of 2022

--- 2022/src/days/day3.rs
+++ 2022/src/days/day3.rs
@@ -1,7 +1,43 @@
-pub fn part1(_fp: &str) -> Option<String> {
-    Some("".to_owned())
+use crate::utils;
+
+pub fn part1(fp: &str) -> Option<String> {
+    let fc = utils::read_file_to_vec(fp).unwrap();
+
+    let mut sum:i32 = 0;
+
+    for l in &fc {
+        let part1 = l[0..l.len()/2].to_owned();
+        let part2 = l[l.len()/2..].to_owned();
+
+        for c in part1.chars() {
+            if part2.chars().into_iter().any(|x|x==c) {
+                sum += c as i32 - if c.is_lowercase() {96} else {64-26};
+                break;
+            }
+        }
+    }
+
+    Some(sum.to_string())
 }
 
-pub fn part2(_fp: &str) -> Option<String> {
-    Some("".to_owned())
+pub fn part2(fp: &str) -> Option<String> {
+    let fc = utils::read_file_to_vec(fp).unwrap();
+
+    let mut sum:i32 = 0;
+
+    for i in (0..fc.len()-2).step_by(3) {
+        let l1 = fc[i].clone();
+        let l2 = fc[i+1].clone();
+        let l3 = fc[i+2].clone();
+
+        for c in l1.chars() {
+            if l2.chars().into_iter().any(|x|x==c) && l3.chars().into_iter().any(|x|x==c){
+                sum += c as i32 - if c.is_lowercase() {96} else {64-26};
+                break;
+            }
+        }
+
+    }
+
+    Some(sum.to_string())
 }
